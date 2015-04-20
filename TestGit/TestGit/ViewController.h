d1a94976d2bfd1ad457eeb6fab4701e9fcd9f3c2
jpt <jpt666@163.com>
2015-04-20 15:17:27 +0800

    create a new project

---
+++ TestGit/TestGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestGit
+//
+//  Created by 你好 on 15/4/20.
+//  Copyright (c) 2015年 jpt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestGit/TestGit/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TestGit
+//
+//  Created by 你好 on 15/4/20.
+//  Copyright (c) 2015年 jpt. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
